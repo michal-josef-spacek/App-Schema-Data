9f75eb6393f8db117a7f02d074dad8a85da2a975
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-09-09 17:29:25 +0200

    Prepare for 0.03 version

--- Data.pm
+++ Data.pm
@@ -8,7 +8,7 @@ use Error::Pure qw(err);
 use Getopt::Std;
 use Unicode::UTF8 qw(decode_utf8);
 
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 # Constructor.
 sub new {
@@ -206,6 +206,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.02
+0.03
 
 =cut
